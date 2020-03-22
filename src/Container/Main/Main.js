82cdabfcea91465cf9d4035d906c71586c5cff59
MZO9400 <hamza.hameed00@yahoo.com>
2020-03-22 18:48:57 +0500

    Dark Mode: some fixes, basic app with text

---
+++ src/Container/Main/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CSS from "./Main.module.css";
+export default class extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>
+          We shape brand identities and design digital experiences to help
+          organizations innovate their business.
+        </h2>
+      </div>
+    );
+  }
+}
