e25e4c04dadb9cef534a48b12d26dc90b808c091
MZO9400 <hamza.hameed00@yahoo.com>
2020-04-10 19:15:26 +0500

    More changes

--- src/Container/Main/AboutUs/AboutUs.js
+++ src/Container/Main/AboutUs/AboutUs.js
@@ -1,38 +1,11 @@
 import React from "react";
-import Typewriter from "typewriter-effect/";
 import CSS from "./AboutUs.module.css";
 
 export default props => (
     <div className={CSS.main} ref={props.refr}>
         <h1>ABOUT THE DOPMENT</h1>
         <div className={CSS.mainAbout}>
-            <div className={CSS.left}>
-                <Typewriter
-                    onInit={typewriter => {
-                        typewriter
-                            .typeString("Hello World!")
-                            .pauseFor(2500)
-                            .deleteAll()
-                            .typeString("We are DOPMENT")
-                            .pauseFor(1000)
-                            .deleteChars(8)
-                            .typeString(" the future!")
-                            .pauseFor(2500)
-                            .deleteAll()
-                            .typeString("Your design")
-                            .pauseFor(1000)
-                            .deleteChars(6)
-                            .typeString("idea")
-                            .pauseFor(1000)
-                            .deleteChars(4)
-                            .typeString("thoughts")
-                            .pauseFor(1000)
-                            .typeString("... Our Implementation!")
-                            .start();
-                    }}
-                    options={{loop: true}}
-                />
-            </div>
+            <div className={CSS.left}/>
             <div className={CSS.right}>
                 <h3>WHO WE ARE</h3>
                 <h2>DOPMENT</h2>
