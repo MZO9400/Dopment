d3dae001b4054995e77d521ced65c70bba9c5520
MZO9400 <hamza.hameed00@yahoo.com>
2020-04-10 14:45:17 +0500

    Updates

---
+++ precache-manifest.f6a3d4792634bbffcddab182ba24b72d.js
@@ -0,0 +1,394 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625fde0166f748b2e1d0a55489213813",
+    "url": "/Dopment/index.html"
+  },
+  {
+    "revision": "c7175e56cf79e5c7c542",
+    "url": "/Dopment/static/css/2.d9ee7e2f.chunk.css"
+  },
+  {
+    "revision": "bc53b337d0153e6a620f",
+    "url": "/Dopment/static/css/main.5b33f6b9.chunk.css"
+  },
+  {
+    "revision": "c7175e56cf79e5c7c542",
+    "url": "/Dopment/static/js/2.c2596a96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dopment/static/js/2.c2596a96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc53b337d0153e6a620f",
+    "url": "/Dopment/static/js/main.726403cf.chunk.js"
+  },
+  {
+    "revision": "ea9cc449db7e0d8cd84e",
+    "url": "/Dopment/static/js/runtime-main.497dfd6e.js"
+  },
+  {
+    "revision": "16d9eb6c65a31cdc3493168be014e983",
+    "url": "/Dopment/static/media/1.16d9eb6c.png"
+  },
+  {
+    "revision": "4dfe2a3737ce577103ea166a348b5cd7",
+    "url": "/Dopment/static/media/1.4dfe2a37.jpg"
+  },
+  {
+    "revision": "69528f4fe201469a5c22a7d8f57c5666",
+    "url": "/Dopment/static/media/1.69528f4f.png"
+  },
+  {
+    "revision": "c2d51620c3ed8e847fc75791cc2aece1",
+    "url": "/Dopment/static/media/1.c2d51620.jpg"
+  },
+  {
+    "revision": "ec1ce443d17615e587024bcacda52d6f",
+    "url": "/Dopment/static/media/1.ec1ce443.jpg"
+  },
+  {
+    "revision": "14981d496b6e69a4d326c68b904a710a",
+    "url": "/Dopment/static/media/10.14981d49.png"
+  },
+  {
+    "revision": "19b04992b5bfe0029a6caa3ad541b2da",
+    "url": "/Dopment/static/media/11.19b04992.jpg"
+  },
+  {
+    "revision": "b471e4ab19b99f6bb617267d0c8057a4",
+    "url": "/Dopment/static/media/11.b471e4ab.png"
+  },
+  {
+    "revision": "1ae8beb95234a1ddb38404bdc86b34ec",
+    "url": "/Dopment/static/media/12.1ae8beb9.jpg"
+  },
+  {
+    "revision": "5f681ba945730b63ade2fc482225d9d6",
+    "url": "/Dopment/static/media/12.5f681ba9.png"
+  },
+  {
+    "revision": "580d0bba78d101ff7663fdea433ddb83",
+    "url": "/Dopment/static/media/13.580d0bba.jpg"
+  },
+  {
+    "revision": "09595666a2586f81a90b13e2dc3d78a9",
+    "url": "/Dopment/static/media/14.09595666.jpg"
+  },
+  {
+    "revision": "19f1f3b19608c0911f44f4cc7ed0eab5",
+    "url": "/Dopment/static/media/14.19f1f3b1.png"
+  },
+  {
+    "revision": "3789723c8f8ccf61e17c4d8496efae29",
+    "url": "/Dopment/static/media/15.3789723c.jpg"
+  },
+  {
+    "revision": "d7c0a621838a3059c7ef715f83e4a24d",
+    "url": "/Dopment/static/media/15.d7c0a621.png"
+  },
+  {
+    "revision": "20cca9b3f9314af65809b1690425c54b",
+    "url": "/Dopment/static/media/16.20cca9b3.png"
+  },
+  {
+    "revision": "bc03bd17eafce0c7bfc6be7743731be0",
+    "url": "/Dopment/static/media/16.bc03bd17.jpg"
+  },
+  {
+    "revision": "95239e4ffeb45a99971a494e109d71ed",
+    "url": "/Dopment/static/media/17.95239e4f.jpg"
+  },
+  {
+    "revision": "ad67c982289076e8cffcf22a10029c0d",
+    "url": "/Dopment/static/media/17.ad67c982.png"
+  },
+  {
+    "revision": "c4a7c7799507ad863a9d91c0494fbdc1",
+    "url": "/Dopment/static/media/18.c4a7c779.jpg"
+  },
+  {
+    "revision": "fefc725949de62b5c6dfed8cddaa9fce",
+    "url": "/Dopment/static/media/18.fefc7259.png"
+  },
+  {
+    "revision": "0a4bb947f0d47f64636af3d591974601",
+    "url": "/Dopment/static/media/19.0a4bb947.png"
+  },
+  {
+    "revision": "cdece14e0cc64c17e36cbb2654e428ff",
+    "url": "/Dopment/static/media/19.cdece14e.jpg"
+  },
+  {
+    "revision": "09d4eaf820cd7b2d9d96894ad015506f",
+    "url": "/Dopment/static/media/1_.09d4eaf8.png"
+  },
+  {
+    "revision": "73272a07985a432952afde37c9a54af1",
+    "url": "/Dopment/static/media/2.73272a07.png"
+  },
+  {
+    "revision": "969ea25f2eafda9813e9024febfac46b",
+    "url": "/Dopment/static/media/2.969ea25f.jpg"
+  },
+  {
+    "revision": "97ef741b85b4b862cf7a01791bc841bb",
+    "url": "/Dopment/static/media/2.97ef741b.jpg"
+  },
+  {
+    "revision": "9867f5f222e251444e4a7a602fa76990",
+    "url": "/Dopment/static/media/2.9867f5f2.jpg"
+  },
+  {
+    "revision": "b135477fdb40eba5da17accf39a7c905",
+    "url": "/Dopment/static/media/2.b135477f.jpg"
+  },
+  {
+    "revision": "00b160d0d0948bb72927c33f67231469",
+    "url": "/Dopment/static/media/20.00b160d0.png"
+  },
+  {
+    "revision": "f24fd4c70300dc35bf8bdd75f01e799e",
+    "url": "/Dopment/static/media/21.f24fd4c7.png"
+  },
+  {
+    "revision": "c00ab9d677b6fd02a3f8dbe8cda0ddc2",
+    "url": "/Dopment/static/media/22.c00ab9d6.png"
+  },
+  {
+    "revision": "b7e2f93a586a84cb256dc0529c4ced24",
+    "url": "/Dopment/static/media/23.b7e2f93a.png"
+  },
+  {
+    "revision": "153db60f02de91a70a98828d83f8952b",
+    "url": "/Dopment/static/media/24.153db60f.png"
+  },
+  {
+    "revision": "edbd56dc567e96984e8c9b5f0495cd49",
+    "url": "/Dopment/static/media/25.edbd56dc.png"
+  },
+  {
+    "revision": "642e59b62e23f825df7fbdf97fb0f173",
+    "url": "/Dopment/static/media/26.642e59b6.png"
+  },
+  {
+    "revision": "6c2605d9a2e4758456fd2a1fea2959c9",
+    "url": "/Dopment/static/media/3.6c2605d9.jpg"
+  },
+  {
+    "revision": "7f20ca396d2cdeb15479ff4ea573dbf3",
+    "url": "/Dopment/static/media/3.7f20ca39.jpg"
+  },
+  {
+    "revision": "9a81f791aa62cb37f7ce2c0b3d4ddd32",
+    "url": "/Dopment/static/media/3.9a81f791.jpg"
+  },
+  {
+    "revision": "d5c1d3d8946de40b9926d29ecc813beb",
+    "url": "/Dopment/static/media/3.d5c1d3d8.png"
+  },
+  {
+    "revision": "052105c9c1d46102d8889ad10145a65e",
+    "url": "/Dopment/static/media/4.052105c9.jpg"
+  },
+  {
+    "revision": "41935bcbaa54a69dc43b3bed7d6792ff",
+    "url": "/Dopment/static/media/4.41935bcb.jpg"
+  },
+  {
+    "revision": "c1e640934c8ccb7f3615b48f9a7461b9",
+    "url": "/Dopment/static/media/4.c1e64093.png"
+  },
+  {
+    "revision": "f83f4290fed197cbef3bdb090532eeb4",
+    "url": "/Dopment/static/media/4.f83f4290.jpg"
+  },
+  {
+    "revision": "0f5618002758367347defc3fee01e6c3",
+    "url": "/Dopment/static/media/5.0f561800.jpg"
+  },
+  {
+    "revision": "33def5bb5f2f6edc492f667891094842",
+    "url": "/Dopment/static/media/5.33def5bb.jpg"
+  },
+  {
+    "revision": "4453beb3cd021bdc3e89f9f40292c0b3",
+    "url": "/Dopment/static/media/5.4453beb3.jpg"
+  },
+  {
+    "revision": "bedda2ba378c02f58ff464fbfdb7b80c",
+    "url": "/Dopment/static/media/5.bedda2ba.png"
+  },
+  {
+    "revision": "7f20ca396d2cdeb15479ff4ea573dbf3",
+    "url": "/Dopment/static/media/5star.7f20ca39.jpg"
+  },
+  {
+    "revision": "50c08e6ca704079ea396c19a25ede656",
+    "url": "/Dopment/static/media/6.50c08e6c.jpg"
+  },
+  {
+    "revision": "702f978a2ec5e4de2a4aa7d4ac4b2222",
+    "url": "/Dopment/static/media/6.702f978a.jpg"
+  },
+  {
+    "revision": "ae671c7f805d0567d93401cf3af9b1c1",
+    "url": "/Dopment/static/media/6.ae671c7f.png"
+  },
+  {
+    "revision": "1ec4276af926166d4f34a600b5bc36a0",
+    "url": "/Dopment/static/media/7.1ec4276a.jpg"
+  },
+  {
+    "revision": "25d63bb5f2a941a8decabba4285206d2",
+    "url": "/Dopment/static/media/7.25d63bb5.jpg"
+  },
+  {
+    "revision": "a074053698011339e0e6c37cb71c6ea1",
+    "url": "/Dopment/static/media/7.a0740536.png"
+  },
+  {
+    "revision": "e52085b6a98307aeeb40473a246ff9f7",
+    "url": "/Dopment/static/media/7.e52085b6.jpg"
+  },
+  {
+    "revision": "052105c9c1d46102d8889ad10145a65e",
+    "url": "/Dopment/static/media/786700d6-d80e-46b7-85bb-d1caafbfcb16.052105c9.jpg"
+  },
+  {
+    "revision": "3cf3caac1c50912a63153b1ab752c5ed",
+    "url": "/Dopment/static/media/8.3cf3caac.jpg"
+  },
+  {
+    "revision": "3d291dcd09d2b947a4d3a1b524a25f77",
+    "url": "/Dopment/static/media/8.3d291dcd.jpg"
+  },
+  {
+    "revision": "9f8177c8d520f3681f8edb4feb4cd39d",
+    "url": "/Dopment/static/media/8.9f8177c8.jpg"
+  },
+  {
+    "revision": "e6b5ecc5cb8a4258ad32810f66f6de96",
+    "url": "/Dopment/static/media/8.e6b5ecc5.png"
+  },
+  {
+    "revision": "1daf2854c049ce85fb5704ffc54a6dd8",
+    "url": "/Dopment/static/media/9.1daf2854.png"
+  },
+  {
+    "revision": "54e1e6b68123f4c5647e14ef532ce2f1",
+    "url": "/Dopment/static/media/9.54e1e6b6.jpg"
+  },
+  {
+    "revision": "69528f4fe201469a5c22a7d8f57c5666",
+    "url": "/Dopment/static/media/Chicken-Hut_png.69528f4f.png"
+  },
+  {
+    "revision": "1605c12eb4b7316d2dcd5afd8e259a4d",
+    "url": "/Dopment/static/media/D8 logo png.1605c12e.png"
+  },
+  {
+    "revision": "eee34e185fb789a438190cd8fae21fc4",
+    "url": "/Dopment/static/media/DOPMENT.eee34e18.png"
+  },
+  {
+    "revision": "3cf3caac1c50912a63153b1ab752c5ed",
+    "url": "/Dopment/static/media/FA3.3cf3caac.jpg"
+  },
+  {
+    "revision": "1daf2854c049ce85fb5704ffc54a6dd8",
+    "url": "/Dopment/static/media/LOGO (3).1daf2854.png"
+  },
+  {
+    "revision": "ad67c982289076e8cffcf22a10029c0d",
+    "url": "/Dopment/static/media/Logo-png.ad67c982.png"
+  },
+  {
+    "revision": "fefc725949de62b5c6dfed8cddaa9fce",
+    "url": "/Dopment/static/media/Modek_logo_png.fefc7259.png"
+  },
+  {
+    "revision": "5f681ba945730b63ade2fc482225d9d6",
+    "url": "/Dopment/static/media/PrintUrShirts.5f681ba9.png"
+  },
+  {
+    "revision": "00b160d0d0948bb72927c33f67231469",
+    "url": "/Dopment/static/media/Reautoparts-Logo2.00b160d0.png"
+  },
+  {
+    "revision": "642e59b62e23f825df7fbdf97fb0f173",
+    "url": "/Dopment/static/media/VR2 copy.642e59b6.png"
+  },
+  {
+    "revision": "9bd49a0e2cdedfaa342759b9a3a86d4a",
+    "url": "/Dopment/static/media/WhatsApp Image 2019-12-03 at 2.11.33 PM.9bd49a0e.jpeg"
+  },
+  {
+    "revision": "c2f6b9dd8c6ab7c69c147d3ad1d6867a",
+    "url": "/Dopment/static/media/_1.c2f6b9dd.png"
+  },
+  {
+    "revision": "73272a07985a432952afde37c9a54af1",
+    "url": "/Dopment/static/media/d8.73272a07.png"
+  },
+  {
+    "revision": "d5c1d3d8946de40b9926d29ecc813beb",
+    "url": "/Dopment/static/media/eleganza logo.d5c1d3d8.png"
+  },
+  {
+    "revision": "c1e640934c8ccb7f3615b48f9a7461b9",
+    "url": "/Dopment/static/media/fixist.c1e64093.png"
+  },
+  {
+    "revision": "ae671c7f805d0567d93401cf3af9b1c1",
+    "url": "/Dopment/static/media/h-logo.ae671c7f.png"
+  },
+  {
+    "revision": "a074053698011339e0e6c37cb71c6ea1",
+    "url": "/Dopment/static/media/kazmiz-500x500.a0740536.png"
+  },
+  {
+    "revision": "e6b5ecc5cb8a4258ad32810f66f6de96",
+    "url": "/Dopment/static/media/kazmiz-logo-png.e6b5ecc5.png"
+  },
+  {
+    "revision": "14981d496b6e69a4d326c68b904a710a",
+    "url": "/Dopment/static/media/logo only.14981d49.png"
+  },
+  {
+    "revision": "b471e4ab19b99f6bb617267d0c8057a4",
+    "url": "/Dopment/static/media/logo png.b471e4ab.png"
+  },
+  {
+    "revision": "20cca9b3f9314af65809b1690425c54b",
+    "url": "/Dopment/static/media/logo-mv.20cca9b3.png"
+  },
+  {
+    "revision": "19b04992b5bfe0029a6caa3ad541b2da",
+    "url": "/Dopment/static/media/logoidea.19b04992.jpg"
+  },
+  {
+    "revision": "580d0bba78d101ff7663fdea433ddb83",
+    "url": "/Dopment/static/media/picknBuy_jpg_.580d0bba.jpg"
+  },
+  {
+    "revision": "0a4bb947f0d47f64636af3d591974601",
+    "url": "/Dopment/static/media/pnglogo.0a4bb947.png"
+  },
+  {
+    "revision": "c00ab9d677b6fd02a3f8dbe8cda0ddc2",
+    "url": "/Dopment/static/media/sams.c00ab9d6.png"
+  },
+  {
+    "revision": "93e5585ff81069bb25c56ee13a79f324",
+    "url": "/Dopment/static/media/ss.93e5585f.png"
+  },
+  {
+    "revision": "153db60f02de91a70a98828d83f8952b",
+    "url": "/Dopment/static/media/taxi.153db60f.png"
+  },
+  {
+    "revision": "edbd56dc567e96984e8c9b5f0495cd49",
+    "url": "/Dopment/static/media/tcp.edbd56dc.png"
+  }
+]);
